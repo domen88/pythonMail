76591f85d03db64d2f7c2fc961389917212c2d3c
Domenico Scotece <domen.88@live.it>
2017-10-26 17:41:41 +0200

    add variable

--- mail.py
+++ mail.py
@@ -15,9 +15,12 @@ def main():
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
 
+    fr = 'x'
+    to = 'x'
+
     msg = MIMEMultipart()
-    msg['From'] = 'x'
-    msg['To'] = 'x'
+    msg['From'] = fr
+    msg['To'] = to
     msg['Subject'] = 'INFRASAFE CHECK'
 
     server.login("x", "x")
@@ -25,7 +28,7 @@ def main():
     body = commands.getoutput('df')
     msg.attach(MIMEText(body, 'plain'))
 
-    server.sendmail("x", "x", msg.as_string())
+    server.sendmail(fr, to, msg.as_string())
     server.quit()
 
 
